1fcf3d1b1ed4d2a62c324005e4cb47adf4b788af
Jorge Vazquez <jorge.avazqq@gmail.com>
2024-10-27 21:14:49 -0600

    Mongo connect

---
+++ backend/data/products.js
@@ -0,0 +1,95 @@
+const products = [
+
+    {
+        _id: '1',
+        name: 'Product 1 elipses text test lorem ipsum',
+        image: '/images/alexa.jpg',
+        description:
+            'lorem ipsum...',
+        brand: 'Brand 1',
+        category: 'Electronics',
+        price: 29.99,
+        countInstock: 0,
+        rating: 4.5,
+        numReviews: 12,
+
+    },
+
+    {
+        _id: '2',
+        name: 'Product 2',
+        image: '/images/airpods.jpg',
+        description:
+            'lorem ipsum...',
+        brand: 'Brand 2',
+        category: 'Electronics',
+        price: 39.99,
+        countInstock: 0,
+        rating: 4,
+        numReviews: 12,
+
+    },
+
+    {
+        _id: '3',
+        name: 'Product 3',
+        image: '/images/camera.jpg',
+        description:
+            'lorem ipsum...',
+        brand: 'Brand 3',
+        category: 'Electronics',
+        price: 19.99,
+        countInstock: 0,        
+        rating: 2,
+        numReviews: 12,
+
+    },
+
+    {
+        _id: '4',
+        name: 'Product 4',
+        image: '/images/sample.jpg',
+        description:
+            'lorem ipsum...',
+        brand: 'Brand 4',
+        category: 'Electronics',
+        price: 9.99,
+        countInstock: 2,
+        rating: 4,
+        numReviews: 4,
+
+    },
+
+    {
+        _id: '5',
+        name: 'Product 5',
+        image: '/images/phone.jpg',
+        description:
+            'lorem ipsum...',
+        brand: 'Brand 5',
+        category: 'Electronics',
+        price: 56.99,
+        countInstock: 0,
+        rating: 4,
+        numReviews: 4,
+
+    },
+
+    {
+        _id: '6',
+        name: 'Product 6',
+        image: '/images/playstation.jpg',
+        description:
+            'lorem ipsum...',
+        brand: 'Brand 6',
+        category: 'Electronics',
+        price: 99.99,
+        countInstock: 1,
+        rating: 4,
+        numReviews: 4,
+
+    },
+
+]
+
+export default products
