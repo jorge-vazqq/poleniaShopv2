b0421aec8d221f4e65f4bb2902ab7c6483f7a569
Jorge Vazquez <jorge.avazqq@gmail.com>
2024-11-02 03:55:25 -0600

    Redux and State management

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import {
   Row,
   Col,
@@ -10,28 +11,23 @@ import {
   Button,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios";
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({});
-
   const { id: productId } = useParams();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
-
-    fetchProduct();
-  }, [productId]);
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
-      <Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{ error?.data?.message || error.error}</Message>
+      ) : ( <Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -77,13 +73,15 @@ const ProductScreen = () => {
                   type="button"
                   disabled={product.countInstock === 0}
                 >
-                  Add To Cart
+                  Añadir al carrito
                 </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
-      </Row>
+      </Row>)}
+      
+     
     </>
   );
 };
