8bb67f8b8da16795f2782709de7649842dc20675
Jorge Vazquez <jorge.avazqq@gmail.com>
2024-12-02 12:29:41 -0600

    placing orders

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -13,7 +13,7 @@ const orderSchema = mongoose.Schema(
         qty: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: String, required: true },
-        name: {
+        product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
           ref: "Product",
